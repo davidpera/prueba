2265ea3b267fad5fc5b285abddcea6f1f3439c6d
David Pérez <david.pe.ra.1996@gmail.com>
2017-09-29 20:31:11 +0200

    Dice suma total

--- sumar.rb
+++ sumar.rb
@@ -2,4 +2,4 @@ print "introduce el primer numero: "
 n1 = gets.chomp.to_i
 print "introduce el 2º numero que viene luego: "
 n2 = gets.chomp.to_i
-puts "La suma vale #{n1 + n2}"
+puts "La suma total vale #{n1 + n2}"
