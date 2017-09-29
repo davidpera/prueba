b12c860e16e6d8d8473581d335591968497a7884
David Pérez <david.pe.ra.1996@gmail.com>
2017-09-29 20:00:32 +0200

    cambia segundo por 2º

--- sumar.rb
+++ sumar.rb
@@ -1,5 +1,5 @@
 print "introduce el primer numero: "
 n1 = gets.chomp.to_i
-print "introduce el segundo numero: "
+print "introduce el 2º numero: "
 n2 = gets.chomp.to_i
 puts "La suma vale #{n1 + n2}"
